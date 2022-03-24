887881aa9ea7d79295d55665cae94eeb80067bce
Chiew <chiews89@gmail.com>
2022-03-24 15:11:27 -0700

    fix about links

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -5,7 +5,7 @@ import "./Footer.css";
 export const Footer = () => {
   return (
     <div className="footer">
-    <ul className="footer-created-with">
+      <ul className="footer-created-with">
         <li className="created-with">Javasctipt</li>
         <li className="created-with">Python</li>
         <li className="created-with">Flask</li>
@@ -13,12 +13,26 @@ export const Footer = () => {
         <li className="created-with">Redux</li>
         <li className="created-with">SQLAlchemy</li>
         <li className="created-with">PostgreSQL</li>
-    </ul>
-    <div className="footer-created-by">
+      </ul>
+      <div className="footer-created-by">
         <p className="footer-owner">Chiew Saetern</p>
-        <a className="github-icon" href="https://github.com/chiews89"><i className="fab fa-github-square"></i></a>
-        <a className="linkedin-icon" href="https://www.linkedin.com/in/chiew-saetern-7a255b95/"><i className="fab fa-linkedin"></i></a>
+        <a
+          className="github-icon"
+          href="https://github.com/chiews89"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-github-square"></i>
+        </a>
+        <a
+          className="linkedin-icon"
+          href="https://www.linkedin.com/in/chiew-saetern-7a255b95/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-linkedin"></i>
+        </a>
+      </div>
     </div>
-  </div>
   );
 };
