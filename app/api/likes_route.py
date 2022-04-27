55cda55e538e27ff6e76ef88a018dc9e1a9ff33a
Chiew <chiews89@gmail.com>
2022-04-27 14:03:10 -0700

    working on likes

--- app/api/likes_route.py
+++ app/api/likes_route.py
@@ -1,3 +1,29 @@
 from flask import Blueprint, request
 from flask_login import login_required, current_user
-from app.models import Comment, User, Post, db
+from app.models import Comment, User, Post, db, Like
+
+like_routes = Blueprint('likes', __name__)
+
+@like_routes.route('/')
+def get_likes():
+    likes = Like.query.all()
+    return {'likes': [like.to_dict() for like in likes]}
+
+@like_routes.route('/', methods=['POST'])
+def add_like():
+    like = Like()
+    likes = request.get_json()
+    like.user_id = likes['user_id']
+    like.post_id = likes['post_id']
+
+    db.session.add(like)
+    db.session.commit()
+    return like.to_dict()
+
+@like_routes.route('/<int:id>', methods=["DELETE"])
+@login_required
+def delete_like(id):
+    delete_like = Like.query.get(id)
+    db.session.delete(delete_like)
+    db.session.commit()
+    return { 'message': "Success" }
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,8 @@
+from flask import Blueprint, request
+from flask_login import current_user, login_required
 from .db import db
 from .user import User
 from .posts import Post
-from .comments import Comment
 from .likes import Like
+
 
