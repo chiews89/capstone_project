4b3a790f91ac28c0a89e173a97b6cfd95b02b30f
Chiew <chiews89@gmail.com>
2022-05-24 16:51:06 -0700

    fixed signup modal

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,22 +1,24 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux'
-import { Redirect } from 'react-router-dom';
-import { signUp } from '../../store/session';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { NavLink, Redirect } from "react-router-dom";
+import { signUp } from "../../store/session";
 
 const SignUpForm = () => {
   const [errors, setErrors] = useState([]);
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
-  const user = useSelector(state => state.session.user);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+  const user = useSelector((state) => state.session.user);
   const dispatch = useDispatch();
 
   const onSignUp = async (e) => {
     e.preventDefault();
-      const data = await dispatch(signUp(username, email, password, repeatPassword));
-      if (data) {
-        setErrors(data)
+    const data = await dispatch(
+      signUp(username, email, password, repeatPassword)
+    );
+    if (data) {
+      setErrors(data);
     }
   };
 
@@ -37,57 +39,69 @@ const SignUpForm = () => {
   };
 
   if (user) {
-    return <Redirect to='/' />;
+    return <Redirect to="/" />;
   }
 
   return (
-    <form className='sign-up-form-modal' onSubmit={onSignUp}>
-      <div className='create-errors'>
+    <form className="sign-up-form-modal" onSubmit={onSignUp}>
+      <div className="create-errors">
         {errors.map((error, ind) => (
           <div key={ind}>{error}</div>
         ))}
       </div>
-      <div>
-        <label className='sign-up-labels'>User Name</label>
-        <input
-        id='sign-up-form-inputs'
-          type='text'
-          name='username'
-          onChange={updateUsername}
-          value={username}
-        ></input>
+      <div className="sign-up-inputs-container">
+        <div>
+          <input
+            className="sign-up-form-inputs"
+            type="text"
+            name="username"
+            placeholder="Username"
+            onChange={updateUsername}
+            value={username}
+          ></input>
+        </div>
+        <div>
+          <input
+            className="sign-up-form-inputs"
+            type="text"
+            name="email"
+            placeholder="Email"
+            onChange={updateEmail}
+            value={email}
+          ></input>
+        </div>
+        <div>
+          <input
+            className="sign-up-form-inputs"
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={updatePassword}
+            value={password}
+          ></input>
+        </div>
+        <div>
+          <input
+            className="sign-up-form-inputs"
+            type="password"
+            name="repeat_password"
+            placeholder="Confirm password"
+            onChange={updateRepeatPassword}
+            value={repeatPassword}
+          ></input>
+        </div>
       </div>
-      <div>
-        <label className='sign-up-labels'>Email</label>
-        <input
-        id='sign-up-form-inputs'
-          type='text'
-          name='email'
-          onChange={updateEmail}
-          value={email}
-        ></input>
-      </div>
-      <div>
-        <label className='sign-up-labels'>Password</label>
-        <input
-        id='sign-up-form-inputs'
-          type='password'
-          name='password'
-          onChange={updatePassword}
-          value={password}
-        ></input>
-      </div>
-      <div>
-        <label className='sign-up-labels'>Confirm Password</label>
-        <input
-        id='sign-up-form-inputs'
-          type='password'
-          name='repeat_password'
-          onChange={updateRepeatPassword}
-          value={repeatPassword}
-        ></input>
-      </div>
-      <button className='sign-up-button' type='submit'>Sign Up</button>
+      <button className="sign-up-button" type="submit">
+        Sign Up
+      </button>
+      <div className="home-signup-from-login">
+            <h3 className="no-account">
+              Have an account?
+            </h3>
+            <NavLink to='/' exact={true} className='have-account-log-in'>
+              Log in
+            </NavLink>
+          </div>
     </form>
   );
 };
