31f420b12935bbdb6e250361c5d40bc395964ae0
Chiew <chiews89@gmail.com>
2023-01-09 13:41:07 -0800

    changing to render

--- app/models/posts.py
+++ app/models/posts.py
@@ -1,11 +1,13 @@
-from .db import db
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 from datetime import datetime
 
 class Post(db.Model):
     __tablename__ = 'posts'
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
     image = db.Column(db.String, nullable=False)
     description = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now(), nullable=False)
